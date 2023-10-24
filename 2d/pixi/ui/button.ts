0a92ee203f0a8bf2eacbe638bf9eefb62c22a0f5
Veselin <vesakabgr@gmail.com>
2023-10-24 21:18:52 +0300

    add button view

--- 2d/pixi/ui/button.ts
+++ 2d/pixi/ui/button.ts
@@ -1,7 +1,12 @@
 import UI from "@/core/2d/pixi/ui/ui";
-
+import { Sprite } from "pixi.js";
 class Button extends UI {
     
+    view?: Sprite;
+
+    setView(view: Sprite) {
+        this.view = view;
+    }
 }
 
 export default Button;
