7bfbbb320227ec6de9500b54a5bd04483476916f
Veselin <vesakabgr@gmail.com>
2023-10-27 07:13:43 +0300

    use snake case

--- 2d/pixi/ui/ui.ts
+++ 2d/pixi/ui/ui.ts
@@ -1,7 +1,15 @@
 import Container from "@/core/container";
+import { snakeCase } from "@/core/utils/string.util";
 
 class UI extends Container{
-    
+    protected name: string;
+
+    constructor() {
+        super();
+
+        this.name = snakeCase(this.constructor.name);
+        
+    }
 }
 
 export default UI;
