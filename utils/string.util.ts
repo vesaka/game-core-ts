e36c3e68266214f6e54ff92e578c52304393b834
Veselin <vesakabgr@gmail.com>
2023-10-27 05:05:41 +0300

    added string case converters

--- utils/string.util.ts
+++ utils/string.util.ts
@@ -5,3 +5,15 @@ export const cx = (list: ClassList, classList: ClassList | string = ''): ClassLi
 
     return Object.assign(list, classList) as ClassList;
 };
+
+export const camelCase = (str: string): string => {
+    return str.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+};
+
+export const kebabCase = (str: string): string => {
+    return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+};
+
+export const snakeCase = (str: string): string => {
+    return str.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();
+};
