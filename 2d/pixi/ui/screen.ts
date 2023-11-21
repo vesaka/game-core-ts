0768b6ac5375696604fa4d259ab99b71da2b697a
Veselin <vesakabgr@gmail.com>
2023-11-21 04:17:14 +0200

    develop screen behaviour

--- 2d/pixi/ui/screen.ts
+++ 2d/pixi/ui/screen.ts
@@ -1,7 +1,56 @@
 import UI from "@/core/2d/pixi/ui/ui";
+import { Container, EventMode } from "pixi.js";
+import Button from "./button";
+import Label from "./label";
 
 class Screen extends UI {
     
+
+    protected view: Container;
+
+    protected layers: { [key: string]: Container } = {};
+
+    protected buttons: { [key: string]: Button } = {};
+
+    protected labels: { [key: string]: Label } = {};
+
+    constructor(options: AnyObject = {}) {
+        super(options);
+        
+        this.view = new Container();
+        this.scene.addChild(this.view);
+    }
+
+    get active(): boolean {
+        return this.view.visible;
+    }
+
+    build() {}
+
+    show(mode: EventMode = 'auto') {        
+        this.view.eventMode = mode;
+        this.view.visible = true;
+        this.$emit('screen_shown', this);
+    }
+
+    hide(mode: EventMode = 'none') {
+        this.view.eventMode = mode;
+        this.view.visible = false;
+
+        this.$emit('screen_hidden', this);
+    }
+
+    enable(mode: EventMode = 'auto') {
+        this.view.eventMode = mode;
+    }
+
+    disable(mode: EventMode = 'none') {
+        this.view.eventMode = mode;
+    }
+
+    
+
+
 }
 
 export default Screen;
