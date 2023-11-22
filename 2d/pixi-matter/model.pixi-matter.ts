68fa2a9e65846ba454f1d0605d2fa46279d52856
Veselin <vesakabgr@gmail.com>
2023-11-22 08:53:32 +0200

    use model name

--- 2d/pixi-matter/model.pixi-matter.ts
+++ 2d/pixi-matter/model.pixi-matter.ts
@@ -1,8 +1,24 @@
 import Container from "@/core/container";
 import { DisplayObject } from "pixi.js";
 import { Body } from "matter-js";
-import { PixiMatterModel } from "./interfaces.pm";
+import { snakeCase } from "@/core/utils/string.util";
+
 class Model extends Container implements ModelInterface {
+
+    protected _name: string = 'model';
+
+    constructor(options: ModelOptions) {
+        super();
+
+        this._name = snakeCase(this.constructor.name);
+        
+        this.applyFilters(options);
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
     createBody(): Body {
         throw new Error("Method not implemented.");
     }
