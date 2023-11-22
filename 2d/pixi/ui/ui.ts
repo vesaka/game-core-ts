0cab799462aa26a89559e0f140e37c7f1efd66cf
Veselin <vesakabgr@gmail.com>
2023-11-22 08:51:10 +0200

    add filters

--- 2d/pixi/ui/ui.ts
+++ 2d/pixi/ui/ui.ts
@@ -4,11 +4,40 @@ import { snakeCase } from "@/core/utils/string.util";
 class UI extends Container{
     protected name: string;
 
-    constructor() {
+    constructor(options: AnyObject = {}) {
         super();
 
         this.name = snakeCase(this.constructor.name);
+
+        this.applyFilters(options);
         
+    }
+
+    filter_position(position: Vector2D): Vector2D {
+        const { screen } = this.app;
+
+        if (position.x >= 0 && position.x <= 1) {
+            position.x *= screen.width;
+        }
+
+        if (position.y >= 0 && position.y <= 1) {
+            position.y *= screen.height;
+        }
+
+        return position;
+    }
+
+    filter_size(size: Size2D): Size2D {
+        const { screen } = this.app;
+
+        for (let key in ['width', 'height']) {
+            const attribute = key as keyof Size2D;
+            if (size.hasOwnProperty(attribute) && size[attribute] >= 0 && size[attribute] <= 1) {
+                size[attribute] *= screen[attribute];
+            }
+        }
+
+        return size;
     }
 }
 
