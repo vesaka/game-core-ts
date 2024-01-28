d5e1313ed3e21e72a1d72a0d2e2ef1f1eda784f2
Veselin <vesakabgr@gmail.com>
2024-01-28 23:20:12 +0200

    add abstract createView method

--- 2d/pixi/ui/button.ts
+++ 2d/pixi/ui/button.ts
@@ -1,13 +1,20 @@
 import UI from "@/core/2d/pixi/ui/ui";
-import { DisplayObject } from "pixi.js";
+import { Graphics } from "pixi.js";
 
 abstract class Button extends UI {
     
-    view?: DisplayObject;
-
-    setView(view: DisplayObject) {
-        this.view = view;
+    view: Graphics;
+  
+    constructor(options: AnyObject) {
+        super(options);
+        this.view = this.createView();
     }
+
+    setView(view: Graphics) {
+        this.view = view;
+    } 
+
+    abstract createView(): Graphics;
 }
 
 export default Button;
