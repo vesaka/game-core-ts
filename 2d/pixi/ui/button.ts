e71fa24ceb53b068dca21a7e9c98f86cb7fa92ec
Veselin <vesakabgr@gmail.com>
2024-02-25 01:36:48 +0200

    use generics

--- 2d/pixi/ui/button.ts
+++ 2d/pixi/ui/button.ts
@@ -1,20 +1,12 @@
 import UI from "@/core/2d/pixi/ui/ui";
 import { Graphics } from "pixi.js";
 
-abstract class Button extends UI {
-    
-    view: Graphics;
-  
-    constructor(options: AnyObject) {
+abstract class Button<V = Graphics> extends UI<UiOptions, V> {
+      
+    constructor(options: UiOptions) {
         super(options);
-        this.view = this.createView();
     }
 
-    setView(view: Graphics) {
-        this.view = view;
-    } 
-
-    abstract createView(): Graphics;
 }
 
 export default Button;
