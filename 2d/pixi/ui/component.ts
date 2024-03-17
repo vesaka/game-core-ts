32b3d6eadfebcd3ffefb66baa6cae3cf1e09b621
Veselin <vesakabgr@gmail.com>
2024-03-17 06:50:08 +0200

    refactor types

--- 2d/pixi/ui/component.ts
+++ 2d/pixi/ui/component.ts
@@ -1,30 +1,26 @@
 import UI from "@/core/2d/pixi/ui/ui";
 
-import { Graphics, Text } from "pixi.js";
+import { Graphics } from "pixi.js";
 
-class Component<T = UiOptions> extends UI<T, Graphics> {
+class Component<T = UiOptions, G = Graphics> extends UI<T, G> {
 
     
     constructor(options: T, bounds?: Size2D) {
         super(options, bounds);
-        this.view.name = this.name;
+        
     }
 
-    createView(): Graphics {
+    createView(): G {
         const view = new Graphics();
         view.beginFill(this.fill, this.alpha);
         view.drawRect(0, 0, this.size.width, this.size.height);
         view.endFill();
-        return view;
+        return view as G;
     }
 
-    updateText(message: string) {
-        const text = new Text(message, this.style);
-        text.position.set(this.size.width * 0.5, this.size.height * 0.5);
-        text.anchor.set(0.5);
-        this.view.addChild(text);
+    update() {
+        this.view = this.createView();
     }
-
     
 }
 
