7276c01003e55fb94f4a4c202abb722563e7dc08
Veselin <vesakabgr@gmail.com>
2024-08-23 19:53:39 +0300

    created tab component

--- 2d/pixi/ui/tabs/tabs.component.ts
+++ 2d/pixi/ui/tabs/tabs.component.ts
@@ -1,7 +1,22 @@
 import Component from "@/core/2d/pixi/ui/component";
+import TabComponent from "./tab.component";
 
-class TabsComponent extends Component {
+class TabsComponent<T = TabComponent> extends Component<T> {
     
+    protected tabs: T[] = [];
+
+    // protected activeTab: T;
+
+    protected activeTabId: number = 0;
+
+    setActiveTab(tabId: number) {
+        this.activeTabId = tabId;
+        this.activeTab = this.tabs[tabId];
+    }
+
+    setup() {
+
+    }
 }
 
 export default TabsComponent;
