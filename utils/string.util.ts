f07aa6d5998dc1d982afee3c7b00876168be614f
Veselin <vesakabgr@gmail.com>
2024-09-01 08:17:49 +0300

    added pluralize and singularize

--- utils/string.util.ts
+++ utils/string.util.ts
@@ -25,3 +25,62 @@ export const pascalCase = (str: string): string => {
 export const ucfirst = (str: string): string => {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export const oddPluralWords: AnyObject = {
+    'child': 'children',
+    'person': 'people',
+    'goose': 'geese',
+    "man": "men",
+    "woman": "women",
+    "tooth": "teeth",
+    "foot": "feet",
+    "mouse": "mice",
+    "louse": "lice",
+    "cactus": "cacti",
+    "focus": "foci",
+    "fungus": "fungi",
+    "nucleus": "nuclei",
+    "syllabus": "syllabi",
+    "phenomenon": "phenomena",
+    "criterion": "criteria",
+    "datum": "data",
+    "medium": "media",
+    "symposium": "symposia",
+    "bacterium": "bacteria",
+    "curriculum": "curricula",
+    "memorandum": "memoranda"
+}
+
+export const pluralize = (str: string): string => {
+    if (/(is|us)$/.test(str)) {
+        return str.replace(/(is|us)$/, 'es');
+    } else if (str.endsWith('s')) {
+        return str;
+    } else if (/(x|s|z|sh|ch|o)/.test(str)) {
+        return str + 'es';
+    } else if (/[^aeiou]y$/.test(str)) { // give me some examples of words that end in y but are not preceded by a vowel
+        return str.slice(0, -1) + 'ies';
+    } else if (/(f|fe)$/.test(str)) {
+        return str.replace(/(f|fe)$/, 'ves');
+    } else if (oddPluralWords[str]) {
+        return oddPluralWords[str];
+    } else if (/(on)$/.test(str)) {
+        return str + 'a';
+    } else if (/(um)$/.test(str)) {
+        return str.replace(/um$/, 'a');
+    }
+    return str + 's';
+}
+
+export const singularize = (str: string): string => {
+    if (/(x|s|z|sh|ch|o)es$/.test(str)) {
+        return str.slice(0, -2);
+    } else if (/[^aeiou]ies$/.test(str)) {
+        return str.slice(0, -3) + 'y';
+    } else if (/ves$/.test(str)) {
+        return str.replace(/ves$/, 'f');
+    } else if (oddPluralWords[str]) {
+        return oddPluralWords[str];
+    }
+    return str.slice(0, -1);
+}
