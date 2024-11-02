ecefaf9503b8a09d187295ef74c412b1c9613ad4
Veselin <vesakabgr@gmail.com>
2024-11-02 10:31:41 +0200

    fix to pass tests

--- utils/string.util.ts
+++ utils/string.util.ts
@@ -7,19 +7,27 @@ export const cx = (list: ClassList, classList: ClassList | string = ''): ClassLi
 };
 
 export const camelCase = (str: string): string => {
-    return str.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+    return str
+        .replace(/[-_\s]+(.)?/g, (_, c) => c ? c.toUpperCase() : '')
+        .replace(/^\w/, c => c.toLowerCase()); 
 };
 
 export const kebabCase = (str: string): string => {
-    return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+    return str.trim()
+        .replace(/([a-z])([A-Z])/g, '$1-$2')
+        .replace(/[_\s]+/g, '-')
+        .toLowerCase();
 };
 
 export const snakeCase = (str: string): string => {
-    return str.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase();
+    return str.trim()
+        .replace(/([a-z])([A-Z])/g, '$1_$2')
+        .replace(/[-\s]+/g, '_')
+        .toLowerCase();
 };
 
 export const pascalCase = (str: string): string => {
-    return str.replace(/(\w)(\w*)/g, (_, g1, g2) => g1.toUpperCase() + g2.toLowerCase());
+    return ucfirst(camelCase(str));
 };
 
 export const ucfirst = (str: string): string => {
@@ -52,19 +60,19 @@ export const oddPluralWords: AnyObject = {
 }
 
 export const pluralize = (str: string): string => {
-    if (/(is|us)$/.test(str)) {
+    if (oddPluralWords[str]) {
+        return oddPluralWords[str];
+    } else if (/(is|us)$/.test(str)) {
         return str.replace(/(is|us)$/, 'es');
+    } else if (/(f|fe)$/.test(str)) {
+        return str.replace(/(f|fe)$/, 'ves');
     } else if (str.endsWith('s')) {
         return str;
-    } else if (/(x|s|z|sh|ch|o)/.test(str)) {
+    } else if (/(x|s|z|sh|ch|o)$/.test(str)) {
         return str + 'es';
     } else if (/[^aeiou]y$/.test(str)) { // give me some examples of words that end in y but are not preceded by a vowel
         return str.slice(0, -1) + 'ies';
-    } else if (/(f|fe)$/.test(str)) {
-        return str.replace(/(f|fe)$/, 'ves');
-    } else if (oddPluralWords[str]) {
-        return oddPluralWords[str];
-    } else if (/(on)$/.test(str)) {
+    }else if (/(on)$/.test(str)) {
         return str + 'a';
     } else if (/(um)$/.test(str)) {
         return str.replace(/um$/, 'a');
@@ -72,15 +80,31 @@ export const pluralize = (str: string): string => {
     return str + 's';
 }
 
+const oddSingularWords: AnyObject = {
+    knives: 'knife',
+    wives: 'wife',
+    wolves: 'wolf',
+    analyses: 'analysis',
+    criteria: 'criterion',
+}
+
 export const singularize = (str: string): string => {
+    for (const [key, value] of Object.entries(oddPluralWords)) {
+        if (value === str) {
+            return key;
+        }
+    } 
+    
+    if (oddSingularWords[str]) {
+        return oddSingularWords[str];
+    }
+    
     if (/(x|s|z|sh|ch|o)es$/.test(str)) {
         return str.slice(0, -2);
     } else if (/[^aeiou]ies$/.test(str)) {
         return str.slice(0, -3) + 'y';
     } else if (/ves$/.test(str)) {
         return str.replace(/ves$/, 'f');
-    } else if (oddPluralWords[str]) {
-        return oddPluralWords[str];
     }
     return str.slice(0, -1);
 }
